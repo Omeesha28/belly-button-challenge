a6fefe357cf024a3c1f1bb4e2a60025e15bc0504
Omeesha28 <esha.patel1095@gmail.com>
2024-02-09 17:03:14 -0500

    added starter files

---
+++ static/js/app.js
@@ -0,0 +1,16 @@
+const url = 'https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json';
+
+// Fetch the JSON data and console log it
+d3.json(url).then(function(data) {
+    console.log(data);
+});
+
+var data = [
+    {
+      x: ['sample_values'],
+      y: ['otu_ids'],
+      type: 'bar'
+    }
+  ];
+  
+  Plotly.newPlot('myDiv', data);
